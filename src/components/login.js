49eda042a28a761567c6050a077f52b5c2bd8711
arjun <arjun.dubey@sjsu.edu>
2018-03-15 16:38:50 -0700

    routes pacakge installed

--- src/components/login.js
+++ src/components/login.js
@@ -19,10 +19,10 @@ class LoginForm extends React.Component{
               <form>
                 <input placeholder='Username'                   defaultValue={this.state.Username}/> <br />
                 <input placeholder='Password' type='Password'   defaultValue={this.state.Password}/>
-                
+               
                  <br />
                 <button onClick={e =>this.onSubmit(e)} > Submit</button>
-                
+               
 
               </form>
 
